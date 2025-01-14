98904932475d1d419e482d2534c03b92c868d28f
jxxngin <wwd0203@daum.net>
2025-01-14 12:16:38 +0900

    feat: multi-threading 기능 구현

--- src/main/java/SimpleDB.java
+++ src/main/java/SimpleDB.java
@@ -11,6 +11,7 @@ public class SimpleDB {
     private final String project;
 
     private String mode = "none";
+    private Connection conn;
 
     public SimpleDB(String host, String username, String password, String project) {
         this.host = host;
@@ -25,13 +26,14 @@ public class SimpleDB {
 
     public Connection getConnection() throws SQLException {
         String url = DB_PROTOCOL + host + "/" + project;
-        return DriverManager.getConnection(url, username, password);
+        conn = DriverManager.getConnection(url, username, password);
+
+        return conn;
     }
 
     public void run(String query) {
         try (
-                Connection conn = getConnection();
-                var stmt = conn.createStatement()
+                var stmt = getConnection().createStatement()
         ) {
             stmt.execute(query);
         } catch (SQLException e) {
@@ -42,8 +44,7 @@ public class SimpleDB {
 
     public void run(String query, String title, String body, boolean isBlind) {
         try (
-                Connection conn = getConnection();
-                var stmt = conn.prepareStatement(query)
+                var stmt = getConnection().prepareStatement(query)
         ) {
             stmt.setString(1, title);
             stmt.setString(2, body);
@@ -58,5 +59,15 @@ public class SimpleDB {
 
     public Sql genSql() {
         return new Sql(this);
+    }
+
+    public void close() {
+        try {
+            conn.close();
+            System.out.println("DB 연결 종료");
+        } catch (SQLException e) {
+            System.out.println("close() 실행 실패");
+            e.printStackTrace();
+        }
     }
 }
